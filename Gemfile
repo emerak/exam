d2ee8702d59486d8606b03a72982bc854c761019
= <alejandra.cernas@tangosource.com>
2013-09-06 00:47:10 -0500

    Set gemfile coonfig

---
+++ Gemfile
@@ -0,0 +1,33 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'pg'
+gem 'haml'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+group :doc do
+  gem 'sdoc', require: false
+end
+group :test, :development do
+  gem 'rspec-rails', "~> 2.13.2"
+  gem 'pry'
+end
+group :test do
+  gem 'capybara', "2.0.2"
+  gem 'factory_girl'
+  gem 'shoulda'
+  gem 'faker'
+end
+
+
+
+
+
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exam::Application.config.secret_key_base = '09a005dae30a50c61b57e19e8f35fe2004e805ff04ffce3e7b76415556579ed7cda5f1c87de3a24e2c6f030f16738078b8de80a059200d96d5f251565df4eba0'
